8703658fd84ed2a3c5c48d6fcf54d30dbcacccd2
PowerChaos <PowerChaos@users.noreply.github.com>
2021-10-17 01:06:37 +0200

    added copyrights

--- includes/hooks/coinbasecommerce.php
+++ includes/hooks/coinbasecommerce.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+<!#CR>
+************************************************************************************************************************
+*                                                    Copyrigths ©                                                      *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          Authors Names    > PowerChaos                                                                               *
+*          Company Name     > VPS Data                                                                                 *
+*          Company Email    > info@vpsdata.be                                                                          *
+*          Company Websites > https://vpsdata.be                                                                       *
+*                             https://vpsdata.shop                                                                     *
+*          Company Socials  > https://facebook.com/vpsdata                                                             *
+*                             https://twitter.com/powerchaos                                                           *
+*                             https://instagram.com/vpsdata                                                            *
+* -------------------------------------------------------------------------------------------------------------------- *
+*                                           File and License Informations                                              *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          File Name        > <!#FN> coinbasecommerce.php </#FN>                                                       
+*          File Birth       > <!#FB> 2021/10/17 00:56:11.892 </#FB>                                                    *
+*          File Mod         > <!#FT> 2021/10/17 01:04:36.045 </#FT>                                                    *         
+*          File Version     > <!#FV> 0.0.1 </#FV>                                                                      
+*                                                                                                                      *
+</#CR>
+*/
+
+
+
+
 add_hook('ClientAreaPageViewInvoice', 1, function ($vars) {
 
     if ($vars['status'] != "Paid" && $_REQUEST['canceled'] == 'yes') {
